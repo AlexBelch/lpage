f283ce5d9a7faab33c21ac17d92e6fd560b5a9bc
Alexander BELCHITSKIY <oleksandr.belchitskyi@gmail.com>
2024-08-01 22:47:48 +0300

    17. Fix background in footer,         Create logic, styles css         and structure for work carousel education images     modified:   index.html     modified:   index.js     modified:   style.css.

--- index.js
+++ index.js
@@ -96,4 +96,165 @@ langUa.addEventListener("click", toggleLanguage);
 const selectLang = document.getElementById("language-select");
 selectLang.addEventListener("click", toggleLanguage);
 
+// let i = 1;
+// for (let li of carousel.querySelectorAll("li")) {
+//   li.style.position = "relative";
+//   li.insertAdjacentHTML(
+//     "beforeend",
+//     `<span style="position:absolute;left:0;top:0">${i}</span>`
+//   );
+//   i++;
+// }
+
+/* конфигурация */
+let width = 410; // ширина картинки
+let count = 1; // cдвигаемое количество изображений
+
+let list = carousel.querySelector("ul");
+let listElems = carousel.querySelectorAll("li");
+
+let padLeft = 0;
+
+if (education_gallery.clientWidth <= 820) {
+  padLeft = (education_gallery.clientWidth - width) / 2;
+  listElems[0].style.paddingLeft = padLeft + "px";
+} else if (education_gallery.clientWidth <= 1359) {
+  padLeft = (education_gallery.clientWidth - width * 2) / 3;
+  listElems[0].style.paddingLeft = padLeft + "px";
+  listElems[0].style.paddingRight = padLeft + "px";
+} else {
+  padLeft = (education_gallery.clientWidth - width * 3) / 4;
+  listElems[0].style.paddingLeft = padLeft + "px";
+  listElems[0].style.paddingRight = padLeft + "px";
+  listElems[1].style.paddingRight = padLeft + "px";
+}
+
+let position = 0; // положение ленты прокрутки
+let currentNumber = 1;
+
+function prev() {
+  // сдвиг влево
+  if (Math.abs(position) === 0) {
+    position = -(listElems.length * width - width * count);
+    currentNumber = 9;
+  } else {
+    position += width * count;
+    currentNumber = currentNumber - 1;
+  }
+
+  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+
+  if (education_gallery.clientWidth <= 820) {
+    if (currentNumber === 9) {
+      listElems[0].style.paddingLeft = 0;
+      listElems[8].style.paddingLeft = padLeft + "px";
+    } else {
+      listElems[currentNumber].style.paddingLeft = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+    }
+  } else if (education_gallery.clientWidth <= 1359) {
+    if (currentNumber === 9) {
+      listElems[0].style.paddingLeft = 0;
+      listElems[0].style.paddingRight = 0;
+      listElems[8].style.paddingLeft = padLeft + "px";
+      listElems[8].style.paddingRight = padLeft + "px";
+    } else {
+      listElems[currentNumber].style.paddingLeft = 0;
+      listElems[currentNumber].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+    }
+  } else {
+    if (currentNumber === 9) {
+      listElems[0].style.paddingLeft = 0;
+      listElems[0].style.paddingRight = 0;
+      listElems[1].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+    } else {
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+      listElems[currentNumber].style.paddingLeft = 0;
+
+      if (currentNumber < 8) {
+        listElems[currentNumber + 1].style.paddingRight = 0;
+      }
+    }
+  }
+
+  // list.style.transform = "translateX(" + position + "px)";
+}
+
+function next() {
+  // сдвиг вправо
+  position -= width * count;
+  currentNumber = currentNumber + 1;
+
+  if (Math.abs(position) === listElems.length * width) {
+    position = 0;
+    currentNumber = 1;
+  }
+
+  if (education_gallery.clientWidth <= 820) {
+    if (currentNumber === 1) {
+      listElems[8].style.paddingLeft = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+    } else {
+      padLeft = (education_gallery.clientWidth - width) / 2;
+      listElems[currentNumber - 2].style.paddingLeft = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+    }
+  } else if (education_gallery.clientWidth <= 1359) {
+    if (currentNumber === 1) {
+      listElems[7].style.paddingLeft = 0;
+      listElems[7].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+    } else {
+      listElems[currentNumber - 2].style.paddingLeft = 0;
+      listElems[currentNumber - 2].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+    }
+  } else {
+    if (currentNumber === 1) {
+      listElems[8].style.paddingLeft = 0;
+      listElems[8].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+      listElems[currentNumber].style.paddingRight = padLeft + "px";
+    } else {
+      listElems[currentNumber - 2].style.paddingLeft = 0;
+      listElems[currentNumber - 2].style.paddingRight = 0;
+      listElems[currentNumber - 1].style.paddingLeft = padLeft + "px";
+      listElems[currentNumber - 1].style.paddingRight = padLeft + "px";
+      if (currentNumber < 9) {
+        listElems[currentNumber].style.paddingRight = padLeft + "px";
+      }
+    }
+  }
+
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+  // list.style.transform = "translateX(" + position + "px)";
+}
+
+carousel.querySelector(".prev").onclick = function () {
+  prev();
+};
+carousel.querySelector(".prevMobile").onclick = function () {
+  prev();
+  carousel.querySelector(".number_image").textContent = currentNumber + "/9";
+};
+
+carousel.querySelector(".next").onclick = function () {
+  next();
+};
+carousel.querySelector(".nextMobile").onclick = function () {
+  next();
+  carousel.querySelector(".number_image").textContent = currentNumber + "/9";
+};
+
 console.log("finish load js");
